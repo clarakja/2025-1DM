1ca386126fea85efbaf8403f40109787320486bd
clarakja <33611179+clarakja@users.noreply.github.com>
2025-01-13 17:54:20 +0900

    Update primeWithExplain.py
    들여쓰기 오류 수정

--- primeWithExplain.py
+++ primeWithExplain.py
@@ -40,7 +40,7 @@ if menu == "소수 판별":
 
     if st.button("소수 여부 확인"):
         is_prime, reason = is_prime_with_reason(number)
-                    st.success(f"{number}은(는) 소수입니다! \\n 이유: {reason}")
+            st.success(f"{number}은(는) 소수입니다! \\n 이유: {reason}")
         else:
             st.error(f"{number}은(는) 소수가 아닙니다.  \\n 이유: {reason}")
 # 소수 생성
